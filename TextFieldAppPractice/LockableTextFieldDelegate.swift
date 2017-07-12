4b6d63ca498bf7fa24b3301f4858615897db8555
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2017-07-12 11:07:30 -0700

    Add view controller delegates

---
+++ TextFieldAppPractice/LockableTextFieldDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  LockableTextFieldDelegate.swift
+//  TextFieldAppPractice
+//
+//  Created by Tomas Sidenfaden on 7/12/17.
+//  Copyright © 2017 Tomas Sidenfaden. All rights reserved.
+//
+
+import Foundation
